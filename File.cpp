9cb8d4c2ebe19135acfee01ced186127cfe39f59
Nguyễn Bá Khoa <khoa.nguyenba@outlook.com>
2023-05-30 11:23:30 +0700

    halfway file

--- File.cpp
+++ File.cpp
@@ -1,9 +1,59 @@
 #include "File.h"
 
-void saveToFile(FlightList list)
+void saveToFile(FlightList* list)
 {
-  //	ofstream fo(DATA_FILE)
+  	ofstream fo(DATA_FILE.c_str());
+  	
+  	if (fo.is_open()) 
+	{
+		for (int i = 0; i < list->list.size(); i++)
+  			list->list[i]->saveToFile(fo);
+  		fo.close();
+	}
+  	else 
+  		cout << "File not found";
 }
-void saveToTempFile(FlightList list);
-void readFromFile(FlightList &list);
-void readFromTempFile(FlightList &list);
+void saveToTempFile(FlightList* list)
+{
+	ofstream fo;
+  	fo.open(TEMP_FILE.c_str());
+  	
+  	if (fo.is_open()) 
+	{
+		for (int i = 0; i < list->list.size(); i++)
+  			list->list[i]->saveToFile(fo);
+  		fo.close();
+	}
+  	else 
+  		cout << "File not found";
+}
+void readFromFile(FlightList*& list)
+{
+	ifstream fi(DATA_FILE.c_str());
+  	Flight* toRead;
+  	if (fi.is_open()) 
+	{
+		for (int i = 0; i < list->list.size(); i++)
+			while (!fi.eof()) {
+				
+			}
+  				toRead->readFromFile(fi);
+  		fi.close();
+	}
+  	else 
+  		cout << "File not found";
+}
+void readFromTempFile(FlightList*& list)
+{
+	ifstream fi(TEMP_FILE.c_str());
+  	
+  	if (fi.is_open()) 
+	{
+		for (int i = 0; i < list->list.size(); i++)
+			while (!fi.eof())
+  				list->list[i]->readFromFile(fi);
+  		fi.close();
+	}
+  	else 
+  		cout << "File not found";
+}
--- File.h
+++ File.h
@@ -1,17 +1,18 @@
 #ifndef FILE_H
 #define FILE_H
 #include <iostream>
+#include <fstream>
 #include "FlightList.h"
 
 using namespace std;
 
-const string DATA_FILE = "data.txt";
-const string TEMP_FILE = "temp.txt";
-const string CSV_FILE = "export.csv";
+const string DATA_FILE = "./data/data.txt";
+const string TEMP_FILE = "./data/temp.txt";
+const string CSV_FILE = "./data/export.csv";
 
-void saveToFile(FlightList list);
-void saveToTempFile(FlightList list);
-void readFromFile(FlightList &list);
-void readFromTempFile(FlightList &list);
+void saveToFile(FlightList* list);
+void saveToTempFile(FlightList* list);
+void readFromFile(FlightList*& list);
+void readFromTempFile(FlightList*& list);
 
 #endif
